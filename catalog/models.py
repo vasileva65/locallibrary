3a79b1dce7e6b8e0c3f16c2655eb01426890add3
Shtosh-qw0 <125343361+Shtosh-qw0@users.noreply.github.com>
2023-11-08 12:18:23 +0500

    .

--- catalog/models.py
+++ catalog/models.py
@@ -10,3 +10,7 @@ class Genre(models.Model):
 class Book(models.Model):
 
     title = models.CharField(max_length=200)
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    summary = models.TextField(max_length=1000, help_text="Enter a brief description of the book")
+    isbn = models.CharField('ISBN', max_length=13, help_text='13 Character')
+    genre = models.ManyToManyField(Genre, help_text="Select a genre fo this book")
