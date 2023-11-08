ef7013ea558ce60e94d3aa237f3def1e2df2713e
Shtosh-qw0 <125343361+Shtosh-qw0@users.noreply.github.com>
2023-11-08 12:14:28 +0500

    Initial commit

---
+++ catalog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Genre(models.Model):
+    
+    name = models.CharField(max_length=200, help_text="Enter a book genre")
+
+    def __str__(self):
+        return self.name
+    
+class Book(models.Model):
+
+    title = models.CharField(max_length=200)
