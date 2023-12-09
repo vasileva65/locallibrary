21562134762905240226a796a17f21be2c6fa0c5
vasileva65 <vasilieva.alena65@gmail.com>
2023-12-09 10:08:16 +0500

    index

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -23,3 +26,6 @@ urlpatterns = [
 urlpatterns += [
     path('catalog/', include('catalog.urls'))
 ]
+urlpatterns += [
+    path('', RedirectView.as_view(url='catalog/')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
